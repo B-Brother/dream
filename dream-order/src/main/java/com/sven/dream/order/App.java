55e52880a1163cf9722f1b973e47b15cf1d165c0
B-Brother <464248978@qq.com>
2015-08-04 17:27:24 +0800

    基本框架

---
+++ dream-order/src/main/java/com/sven/dream/order/App.java
@@ -0,0 +1,13 @@
+package com.sven.dream.order;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
